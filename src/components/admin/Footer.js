fc576cf8e7083e0daa4d33e59ce9f7dc19914f40
Mradul Sharma <15956546+ermradulsharma@users.noreply.github.com>
2026-01-26 17:34:12 +0530

    feat: Add admin footer component with copyright notice.

--- src/components/admin/Footer.js
+++ src/components/admin/Footer.js
@@ -1,9 +1,7 @@
 export default function Footer() {
     return (
         <footer className="py-3 px-8 text-center border-t border-gray-200 mt-auto bg-gray-50/50">
-            <p className="text-sm text-gray-500">
-                &copy; {new Date().getFullYear()} PahadiGo. All rights reserved.
-            </p>
+            <p className="text-sm text-gray-500 text-end">&copy; {new Date().getFullYear()} PahadiGo. All rights reserved.</p>
         </footer>
     );
 }
